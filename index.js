5f04ab600dc746ef212db29774af292c81c081dc
Hayden James Cooper <haydencpr95@gmail.com>
2023-05-02 14:52:01 -0400

    Random/ Full text Render from json for recipe

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const mainRecipes = require('./assets/recipes/main/recipe.json');
 
 const PORT = process.env.PORT || 3002;
 
-app.get('/testRecipe', (request, response) => {
-
-  response.status(200).send(mainRecipes);
-
+app.get('/testRecipe/random', (request, response) => {
+  const randomIndex = Math.floor(Math.random() * mainRecipes.recipes.length);
+  response.status(200).send(mainRecipes.recipes[randomIndex]);
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
